1f2a912ed7c16714c0f0a0d333432c5c4ff77d78
lanhaitunyaojiejie <183156697@qq.com>
2016-01-12 15:14:59 +0800

    111

---
+++ _runtime/datacache/a/a/TS4_aadf96a040b756db1b62e4245261d1cf.php
@@ -0,0 +1,7 @@
+<?php
+//0000000000002d535546d99dbbe84f4bf9918a1a5c61x^U’Ñnƒ0Eÿ%ï“š
+˜Çmû”BÚY‚HÐ:Uý÷ÙÐí177¶o,á õjÛO÷f£UµSÀáP(@M>Ä;U#hRJPßÏv•L’Ä´$×‘¤TÄØ;>g©NëÇèÆÈJN
+õ”GqHÓÔì¦þ§iý’¬Ë–m÷TKV3P½¥
+éÅÚ¼âêþçfë èªÃ«R\3Ð‘Fõ“†«úöKÎLBsÎ®õÃàÆ´ÆáÞ»º#hæ¢šÎõbf*b¿8×m;"š0mûþ‰¥™‹
+Ø))À<¤L³Å {2LAí/³ä¼†Bû fM…Á±s7ñ­0"48\EÚÀæ\KH¦]¢Ç±UõY’'[3ÑbþÿŠGÊGþÏÇ¾ê,7yi²J‚¢Tåúý6áL¨­êÇ/ßÆ¦
+?>
---
+++ _runtime/filecache/TS4__xdata_lget_admin_Mobile.php
@@ -0,0 +1,13 @@
+<?php
+return array (
+  'setting' => 
+  array (
+    'switch' => '1',
+  ),
+  'w3gLogo' => 
+  array (
+    'attach' => '',
+    'logo' => '86',
+  ),
+);
+?>
